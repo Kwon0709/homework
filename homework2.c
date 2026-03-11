e3515cbc10d395578d8e231e1ca11763c1bc299d
Kwon0709 <ppiqq7525@gmail.com>
2026-03-12 06:34:56 +0900

    Update homework2.c

--- homework2.c
+++ homework2.c
@@ -5,13 +5,19 @@ int main() {
 	double x = 0;
 	short bit = 0;
 
-	printf("Plz enter real number: ");
+	printf("Plz enter real number: "); //실수 입력
+
 	if (scanf("%lf", &x) != 1) {
-		printf("error!");
+		printf("error!"); //실수를 정상적으로 입력 받지 못했을 경우 에러문구 띄우고 시스템 종료
 		return 1;
 	}
 
-	bit = (short)(x * 256);
+	if (x < -128 || x>127) {
+		printf("Out of range!");//Q8.8범위에서 벗어날경우 범위에서 벗어남을 알려주고 시스템 종료
+		return 1;
+	}
+
+	bit = (short)(x * 256);//실수를 고정소수점으로 변환
 
 	for (int i = 15;i >= 0;i--) {
 		printf("%d", (bit >> i) & 1);
