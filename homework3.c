50c098c1179b475428b8d861f511e30ada0e2d9b
Kwon0709 <ppiqq7525@gmail.com>
2026-03-15 22:15:31 +0900

    Update homework3.c

--- homework3.c
+++ homework3.c
@@ -4,7 +4,7 @@
 int main() {
 	float num = 0;
 	int sign = 0;
-	int square = 0; //정규화과정에서 발생하는 지수
+	int square = 0; //정규화과정에서 발생하는 지수를 받기위한 변수
 	int bias;
 	int mantissa[23] = { 0 };
 
@@ -27,11 +27,11 @@ int main() {
 		square--;
 	}
 
-	bias = 127 + square; 
+	bias = 127 + square;
 
 	num -= 1.0; //가수를 구하기 위해 소수로 변경
 
-	for (int k = 0;k < 23;k++) //소수부분 이진수로 변환
+	for (int k = 0; k < 23; k++) //소수부분 이진수로 변환
 	{
 		num *= 2;
 		if (num >= 1) {
@@ -45,16 +45,16 @@ int main() {
 
 	printf("%d.", sign); //부호에 따른 0 or 1 출력
 
-	for (int i = 7;i >=0;i--)//지수부분출력
+	for (int i = 7; i >= 0; i--)//지수부분출력
 	{
 		printf("%d", (bias >> i) & 1);
 	}
 	printf(".");
 
-	for (int j=0;j<23;j++) //가수부분 출력
+	for (int j = 0; j < 23; j++) //가수부분 출력
 	{
 		printf("%d", mantissa[j]);
 	}
 
-return 0;
+	return 0;
 }
