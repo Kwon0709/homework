f3ce5bd7fc9b092dcec3b9d899ca476cced921c4
kolon <5819539@stu.kmu.ac.kr>
2026-03-28 23:58:52 +0900

    Update my_fibonacci.h

--- fibonacci/my_fibonacci.h
+++ fibonacci/my_fibonacci.h
@@ -1,4 +1,4 @@
-typedef unsigned long long my_fibonacci;
+typedef unsigned long long my_fibonacci; //피보나치수열 f(n)이 f(47)이상으로 가면 int형에서는 오버플로 발생, 따라서 long long형 사용하여 계산 범위를 더 크게 늘려줌
 
 extern my_fibonacci find_Gcd(my_fibonacci i, my_fibonacci j);
 extern my_fibonacci fibonacci(my_fibonacci n);
