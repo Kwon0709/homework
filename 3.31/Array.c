6a16db77885068a22d0e4c7671eb72e3fa6e9aa5
kolon <5819539@stu.kmu.ac.kr>
2026-04-03 16:16:27 +0900

    Update Array.c

--- 3.31/Array.c
+++ 3.31/Array.c
@@ -1,37 +1,37 @@
 #include<stdio.h>
 #include<stdlib.h>
 #include<time.h>
-#define SIZE 20
+#define N 20
 
 int main() {
-	int Array[SIZE];
+	int Array[N];
 
 	srand(time(NULL)); //매번 다른 랜덤값 호출
 
 	printf("=======================================\n");
-	for (int i = 0; i < SIZE;i++) {
-		Array[i] = rand() % (SIZE + 1);
-	
+	for (int i = 0; i < N;i++) {
+		Array[i] = rand() % (N + 1);
+
 		printf("{%d}\t", Array[i]);
-		if ((i+1) % 5 == 0) printf("\n");
+		if ((i + 1) % 5 == 0) printf("\n");
 	}
 
 	printf("\n");
 
 	printf("=======================================\n");
 
-	for (int j = SIZE-1; j >= 0; j--) {
+	for (int j = N - 1; j >= 0; j--) {
 		printf("{%d}\t", Array[j]);
-		if ((SIZE - j) % 5 == 0) printf("\n");
+		if ((N - j) % 5 == 0) printf("\n");
 	}
 
 	printf("\n");
 
 	int Sum = 0;
-	int Max=Array[0];
-	int Min=Array[0];
+	int Max = Array[0];
+	int Min = Array[0];
 
-	for (int i = 0; i < SIZE; i++) {
+	for (int i = 0; i < N; i++) {
 		Sum += Array[i];
 
 		if (Max < Array[i]) { Max = Array[i]; }
