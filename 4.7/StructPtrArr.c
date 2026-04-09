dbcf126031cf96260ccaba5b2719a01a15d01b29
kolon <5819539@stu.kmu.ac.kr>
2026-04-09 21:32:47 +0900

    Create StructPtrArr.c

---
+++ 4.7/StructPtrArr.c
@@ -0,0 +1,79 @@
+#define _CRT_SECURE_NO_WARNINGS
+#include<stdio.h>
+#include<stdlib.h>
+#include<string.h>
+#include<time.h>
+
+typedef struct {
+	char name [100];
+	int studentNum;
+	int score;
+}students;
+
+int main() {
+	int N;
+
+	printf("숫자 입력(범위 1~100): ");
+	scanf("%d", & N);
+
+	if (N > 100 || N<=0) {
+		printf("범위 안으로 입력하세요.\n");
+		return 1;
+	}
+
+
+	students* arr = (students*)malloc(sizeof(students) * N); 
+
+	if (arr == NULL) {
+		printf("메모리 할당 실패\n");
+		return 1;
+	}
+
+	srand((unsigned int)time(NULL));
+
+
+	FILE* fp;
+
+
+
+	fp = fopen("C:\\Users\\kwon\\Downloads\\name_list.txt", "r");
+	if (fp == NULL) {
+
+		printf("Fail to open\n");
+
+		return 0;
+	}
+
+	int best = 0;
+	int worst = 0;
+	int aver = 0;
+
+	for (int i = 0; i < N;i++) {
+
+		fgets((*(arr+i)).name, sizeof((*(arr + i)).name), fp);
+		(*(arr + i)).name[strcspn((*(arr + i)).name, "\r\n")] = '\0';
+
+		(*(arr + i)).studentNum = i + 1;
+		(*(arr + i)).score = (rand() % 100) + 1;
+
+		if ((*(arr + i)).score > (*(arr + best)).score) best = i;
+		if ((*(arr + i)).score < (*(arr + worst)).score) worst = i;
+
+		aver += (*(arr + i)).score;
+
+		printf("%s %d %d\n", (*(arr + i)).name, (*(arr + i)).studentNum, (*(arr + i)).score);
+	}
+
+	printf("--------------------------------------------------------\n");
+
+	printf("최고점의 학생 정보:(이름:%s,학번:%d,성적:%d)\n", (*(arr + best)).name, (*(arr + best)).studentNum, (*(arr + best)).score);
+	printf("\n최저점의 학생 정보:(이름:%s,학번:%d,성적:%d)\n", (*(arr + worst)).name, (*(arr + worst)).studentNum, (*(arr + worst)).score);
+
+	printf("\n평균 점수:%.1f\n", (double)aver / N);
+
+	fclose(fp);
+
+	free(arr);
+	
+	return 0;
+}
