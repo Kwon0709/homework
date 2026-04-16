70892f37be96c8a953c26f219081170ab1c96454
kolon <5819539@stu.kmu.ac.kr>
2026-04-16 20:04:20 +0900

    Create myArrayList.c

---
+++ 4.16/myArrayList.c
@@ -0,0 +1,114 @@
+#include<stdio.h>
+#include<stdlib.h>
+
+#include"myArrayList.h"
+
+arrayList* createArrayList(int size) {
+	arrayList* al;
+	al = (arrayList*)malloc(sizeof(arrayList));
+
+	al->data = (elementArrayList*)malloc(sizeof(elementArrayList) * size);
+	al->size = 0;
+	al->capacity = size;
+
+	return al;
+}
+
+void destroyArrayList(arrayList* al) {
+	free(al->data);
+	free(al);
+}
+
+int isEmptyArrayList(arrayList* al) {
+	if (al->size == 0) {
+		return 1;
+	}
+	else {
+		return 0;
+	}
+}
+
+int isFullArrayList(arrayList* al) {
+	if (al->size == al->capacity) {
+		return 1;
+	}
+	else {
+		return 0;
+	}
+}
+
+int sizeArrayList(arrayList* al) {
+	return al->size;
+}
+
+int insertItemArrayList(arrayList* al, int pos, elementArrayList item) {
+	if (item <= 0){
+		printf("양수만 입력해주세요.\n");
+		return 0; 
+	}
+
+	if (pos<0 || pos>al->size) {
+		printf("배열 범위에 맞게 입력해주세요.\n");
+		return 0;
+	}
+
+	if (isFullArrayList(al)) {
+		elementArrayList* tmp = (elementArrayList*)realloc(al->data, sizeof(elementArrayList) * al->capacity * 2);
+		if (tmp == NULL)return 0;
+		al->data = tmp;
+		al->capacity *= 2;
+	}
+
+	for (int i = al->size - 1;i >= pos;i--) {
+		al->data[i + 1] = al->data[i];
+	}
+	al->data[pos] = item;
+	al->size++;
+
+	return 1;
+}
+
+elementArrayList deleteItemArrayList(arrayList* al, int pos) {
+	if (pos<0 || pos>al->size - 1) {
+		return 0;
+	}
+	elementArrayList item = al->data[pos];
+
+	for (int i = pos; i < al->size - 1;i++) {
+		al->data[i] = al->data[i + 1];
+	}
+	al->size--;
+
+	return item;
+}
+
+void initArrayList(arrayList* al) {
+	for (int i = al->size - 1;i >= 0;i--) {
+		deleteItemArrayList(al, i);
+	}
+}
+
+elementArrayList getItemArrayList(arrayList* al, int pos) {
+	return al->data[pos];
+}
+
+int replaceItemArrayList(arrayList* al, int pos, elementArrayList item) {
+	if (item <= 0) {
+		printf("양의 정수만 입력하세요.\n");
+		return 0;
+	}
+
+	if (pos<0 || pos>al->size - 1) {
+		printf("배열 범위에 맞게 입력하세요.\n");
+		return 0;
+	}
+	al->data[pos] = item;
+
+	return 1;
+}
+
+void printArrayList(arrayList* al) {
+	for (int i = 0; i < al->size; i++) {
+		printf("[%d] %d",i, al->data[i]);
+	}
+}
